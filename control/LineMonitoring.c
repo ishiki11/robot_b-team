4adf6ed2e043c6581715391b82c63e63a574db61
t-dounoma <101164515+t-dounoma@users.noreply.github.com>
2023-05-10 09:52:45 +0900

    [feat] LineMonitoringクラスの作成
    - LineMonitoring.c
    - LineMonitoring.h

---
+++ control/LineMonitoring.c
@@ -0,0 +1,10 @@
+#include "ev3api.h"
+#include "LineMonitoring.h"
+#include "ColorSensor.h"
+
+bool is_detexting_line(int thereshold){
+
+}
+bool is_detexts_goal_maeker(int thereshold){
+
+}
---
+++ control/LineMonitoring.h
@@ -0,0 +1,7 @@
+#ifndef ROBOT_B_TEAM_LINEMONITORING_H
+#define ROBOT_B_TEAM_LINEMONITORING_H
+
+bool is_detexting_line(int thereshold);
+bool is_detexts_goal_maeker(int thereshold);
+
+#endif //ROBOT_B_TEAM_LINEMONITORING_H
