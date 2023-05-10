bac46ef750f3ee6fc03289caad9a3ebec8e578f2
t-dounoma <101164515+t-dounoma@users.noreply.github.com>
2023-05-10 09:41:24 +0900

    [feat] Motorクラスの作成
    - Motor.cの作成
    - Motor.hの作成

---
+++ device/Motor.c
@@ -0,0 +1,15 @@
+#include "ev3api.h"
+#include "Motor.h"
+
+void motor_init(){
+
+}
+int motor_spin_volume(){
+  return ;
+}
+void motor_stop(){
+
+}
+void motor_spin(){
+
+}
---
+++ device/Motor.h
@@ -0,0 +1,9 @@
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+void motor_init();
+void motor_spin_volume();
+void motor_stop();
+void motor_spin();
+
+#endif
