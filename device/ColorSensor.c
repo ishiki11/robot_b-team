65187798a5553142059a56bdb0f35c21952e670e
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-05-16 10:49:50 +0900

    ColorSensor作成

--- device/ColorSensor.c
+++ device/ColorSensor.c
@@ -2,6 +2,21 @@
 static const sensor_port_t color_sensor = EV3_PORT_2;
 
 int get_value() {
-  int reflect = ev3_color_sensor_get_reflect(color_sensor);
-  return reflect;
+  int color_value=ev3_color_sensor_get_color(color_sensor);
+  if (color_value==1,color_value=6) {
+    return ev3_color_sensor_get_reflect(color_sensor);
+  } else if (color_value==5) {
+    return 110;
+  }
 }
+
+
+/*
+黒　1
+青　2
+緑　3
+黄　4
+赤　5
+白　6
+茶　7
+*/
