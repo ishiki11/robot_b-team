029764e83e01f562a210245ddf8795492a5f9679
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-05-17 10:24:53 +0900

    Color値から状態を取得

--- control/LineMonitoring.c
+++ control/LineMonitoring.c
@@ -2,19 +2,27 @@
 #include "LineMonitoring.h"
 #include "ColorSensor.h"
 
-static int threshold = 20; 
+static int threshold = 2;
 
 // ラインを検知する
-bool is_detexting_line(int thereshold){
-  int reflect = get_value();
-  if (reflect < threshold) {
+bool is_detexting_line(int threshold) {
+  int value = get_value();
+  if (value < threshold) {
+    // value = 1 黒
     return true;
   } else {
+    // value = 2 白
     return false;
   }
 }
 
 // ゴールの印を検知する
-bool is_detexts_goal_maeker(int thereshold){
-  
+bool is_detexts_goal_marker(int threshold) {
+  int value = get_value();
+  if (value > threshold) {
+    // value = 3 赤
+    return true;
+  } else {
+    return false;
+  }
 }
