744aece8b1f668e39f75036d1e9ec939de82f18d
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-05-17 10:16:15 +0900

    関数コメント追加

--- control/LineMonitoring.h
+++ control/LineMonitoring.h
@@ -1,7 +1,10 @@
 #ifndef ROBOT_B_TEAM_LINEMONITORING_H
 #define ROBOT_B_TEAM_LINEMONITORING_H
 
+// ラインを検知する
 bool is_detexting_line(int);
+
+// ゴールの印を検知する
 bool is_detexts_goal_maeker(int);
 
 #endif //ROBOT_B_TEAM_LINEMONITORING_H
