965bf9a437b1294721f6d2fadf5b96efa47e7b71
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-06-28 10:35:18 +0900

    .hファイルの作成

--- system/Linetracer.h
+++ system/Linetracer.h
@@ -1,9 +1,12 @@
 #ifndef LINE_TRACER_H_
 #define LINE_TRACER_H_
 
+#include "ev3api.h"
+
 /* 公開の関数宣言 */
+// デバイスを初期化する
 void linetracer_init();
-// 走る
-void linetracer_run();
+// 経路に沿って走行する
+void running_traced_line();
 
 #endif
