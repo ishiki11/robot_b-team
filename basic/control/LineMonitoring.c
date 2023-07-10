5637c7fe43761801b719be3eeeccac9380b156ef
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-07-10 10:59:54 +0900

    変更

--- basic/control/LineMonitoring.c
+++ basic/control/LineMonitoring.c
@@ -2,21 +2,22 @@
 #include "LineMonitoring.h"
 #include "ColorSensor.h"
 
+static threshold = 30
 // ラインを検知する
-bool is_detexting_line(int threshold) {
-  int value = get_value();
-  if (value == 1) {
-    // value = 1 黒
+bool is_detexting_line() {
+  int reflect = get_value();
+  if (reflect < threshold) {
+    // 黒
     return true;
   } else {
-    // value = 2 白
+    // 白
     return false;
   }
 }
 
 // ブルーライン検知（ダブルループに変更）
 bool is_detexts_blue_line(int threshold) {
-  int value = get_value();
+  int value = get_color();
   if (value == 3) {
     // value = 3 青
     return true;
--- basic/device/ColorSensor.c
+++ basic/device/ColorSensor.c
@@ -2,9 +2,26 @@
 static const sensor_port_t color_sensor = EV3_PORT_2;
 
 int get_value() {
-  static int reflect = 0;
-
-  reflect = ev3_color_sensor_get_reflect(sensor_port_t color_sensor)
-  
+  int reflect = ev3_color_sensor_get_reflect(sensor_port_t color_sensor)
   return reflect;
+}
+
+int get_color() {
+  int color_value = 0;
+
+  color_id = ev3_color_sensor_get_color(color_sensor);
+  color_value = 0;
+  switch(color_id) {
+    case COLOR_BLACK:
+      color_value = 1;
+      break;
+    case COLOR_WHITE:
+      color_value = 2;
+      break;
+    case COLOR_BLUE:
+      color_value = 3;
+      break;
+  }
+  
+  return color_value;
 }
