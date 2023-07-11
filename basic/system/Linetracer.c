a23b01028f4744083e3810a80b6fbce6e631ce9e
t-dounoma <t.dounomae.sys21@morijyobi.ac.jp>
2023-07-11 10:13:05 +0900

    [update] Linetracer.c
    - 修正

--- basic/system/Linetracer.c
+++ basic/system/Linetracer.c
@@ -3,6 +3,7 @@
 #include "LineMonitoring.h"
 #include "DirectionControl.h"
 #include "DistanceMonitoring.h"
+#include "ColorSensor.h"
 
 static bool_t isline
 
@@ -14,21 +15,35 @@ void linetracer_init() {
 
 // 経路に沿って走行する
 void running_traced_line() {
-  // ライン上かどうか
-  isline = is_detexting_line()
 
   // pid操作量を取得
   float pid[] = get_pid_operating_volume()
 
   reflect = get_value();
   // 向きを取得
-  int direction = calculate_travel_direction(pid, reflect)
-
-  if (isline) {
-    // 黒の時
-    motor_spin(40,30)
-  } else {
-    // 白の時
-    motor_spin(30,40)
+  int tern = calculate_travel_direction(pid, reflect)
+  // 前進のパワーと操作量が100を超えていないか検証する
+  if (tern > 60)
+  {
+    tern = 60;
   }
+  if (tern < -60)
+  {
+    tern = -60;
+  }
+
+  if (tern < 0)
+  {
+    // 右上に前進する
+    lpower = 40 - tern;
+    rpower = 40;
+  }
+  else
+  {
+    // 左上に前進する
+    lpower = 40;
+    rpower = 40 + tern;
+  }
+
+  motor_spin(lpower,rpower)
 }
